c0c1a86be618849702a0401fd48a02da2d69d52f
Ryan Griffin <griffiryan@hooch.ecs.vuw.ac.nz>
2015-06-24 19:19:53 +1200

    hhyytryuioph456nng imgghhghhhhhgg58gtghghtyu56756768hggghhhhhhh567333gggg777ggg

--- web.js
+++ web.js
@@ -338,7 +338,8 @@ app.get('/userBadges',function(req,res){
   // console.log("r"+req.query.username);
 
 
-  var b1,b2,b3,b4;
+  var ab1,ab2,ab3,ab4;
+  var bb1,bb2,bb3,bb4;
 
   //var result;
 
@@ -346,17 +347,32 @@ app.get('/userBadges',function(req,res){
 
 
   query.on('row', function(row, result) {
-      result.addRow(row);
+     // result.addRow(row);
+    ab1 = result.b1;
+    ab2 = result.b2;
+    ab3 = result.b3;
+    ab4 = result.b4;
+    bb1 = row.b1;
+    bb2 = row.b2;
+    bb3 = row.b3;
+    bb4 = row.b4;
+
   });
 
   query.on('end', function(result) {
       console.log(result.rows.length + ' rows were received');
+      console.log("1"+ab1);
+      console.log("2"+ab2);
+      console.log("3"+ab3);
+      console.log("4"+ab4);
+      console.log("5"+bb1);
+      console.log("6"+bb2);
+      console.log("7"+bb3);
+      console.log("8"+bb4);
+
   });
 
-    // b1 = result.b1;
-    // b2 = result.b2;
-    // b3 = result.b3;
-    // b4 = result.b4;
+
 
     // var toReturn = ""+b1.toString()+b2.toString()+b3.toString()+b4.toString();
 
